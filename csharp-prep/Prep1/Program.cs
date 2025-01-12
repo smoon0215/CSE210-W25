1a9816e5a461bc074f0db12f05767cc47d2a3fdd
smoon0215 <moo19034@byui.edu>
2025-01-11 21:02:04 -0700

    Completed Preperation activities 1 and 2

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,18 @@
 using System;
+using System.Reflection.Emit;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}");
+        
     }
+
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+       Console.Write("What is your grade percentage? ");
+       string valueFromUser = Console.ReadLine();
+
+       int grade = int.Parse(valueFromUser);
+
+       string letter = "";
+       
+       if (grade >= 90)
+       {
+        letter = "A";
+       }
+       else if (grade >=80)
+       {
+        letter = "B";
+       }
+       else if (grade >=70)
+       {
+        letter = "C";
+       }
+       else if (grade >=60)
+       {
+        letter = "D";
+       }
+       else
+       {
+        letter = "F";
+       }
+
+       Console.WriteLine($"Your grade is: {letter}");
+
+       if (grade >= 70 )
+       {
+        Console.Write("Congradulations! You passed the class! ");
+       }
+       else
+        {
+            Console.Write("You did not pass the class. You learned, you will get it next time!");
+        }
     }
 }
