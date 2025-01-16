bf566ccb1024a99b5bc6b5c243e8193a77d4d31c
smoon0215 <moo19034@byui.edu>
2025-01-16 15:05:29 -0700

    Completed Preoperation activity 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,32 @@
 using System;
+using System.Diagnostics;
 
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep3 World!");
+    {        
+        Random number = new Random();
+        int magic_number = number.Next(1,100);
+        int user_guess = -1;
+        
+        while (user_guess != magic_number)
+        {
+            Console.WriteLine("What is your guess?");
+            user_guess = int.Parse(Console.ReadLine());
+
+            if (user_guess > magic_number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (user_guess < magic_number)
+            {
+                Console.WriteLine("Higher");
+            } 
+            else
+            {
+                Console.WriteLine("You guessed it! ");
+            }
+        }
+
     }
 }
