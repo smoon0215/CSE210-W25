47baf432f36c37ad1893517f70e08b895dc18a63
smoon0215 <moo19034@byui.edu>
2025-01-16 15:05:46 -0700

    Completed actviity 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,36 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Linq;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        int number = -1; 
+
+        while (number != 0)
+        {
+            Console.WriteLine("Enter a number: ");
+            number = int.Parse(Console.ReadLine());
+            
+            numbers.Add(number);   
+        }
+
+        int list_total = numbers.Sum();
+        int list_avg = list_total / numbers.Count;
+        int max = numbers.Max();
+        int min = numbers.Min();
+        
+        Console.WriteLine($"The sum is: {list_total}");
+        Console.WriteLine($"The average is: {list_avg}");
+        Console.WriteLine($"The maximumn value is {max}");
+        Console.WriteLine($"The minimumn value is {min}");
+
+
+         
     }
 }
